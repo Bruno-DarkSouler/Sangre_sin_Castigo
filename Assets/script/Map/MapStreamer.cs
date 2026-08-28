5998efecacd52c566237b2deb899817dfe944075
Bruno-DarkSouler <brunofornasar@gmail.com>
2026-08-27 21:08:05 -0300

    Se agregaron en la escena de edicion del mapa unos objetos vacios que actuan de indicadores de coordenadas simplemente

---
+++ Assets/script/Map/MapStreamer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapStreamer : MonoBehaviour
+{
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.position.x < -450 && player.position.x > -551)
+        {
+            
+        }
+    }
+}
