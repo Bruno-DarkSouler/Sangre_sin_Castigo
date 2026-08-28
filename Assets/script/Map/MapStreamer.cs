88f6efad8959f2c3a24368f85061a41e9212347b
Bruno-DarkSouler <brunofornasar@gmail.com>
2026-08-28 18:43:39 -0300

    Carga dinamica de la primera parte de la playa

--- Assets/script/Map/MapStreamer.cs
+++ Assets/script/Map/MapStreamer.cs
@@ -6,13 +6,33 @@ public class MapStreamer : MonoBehaviour
 {
 
     public Transform player;
+    public Transform mapGrid;
+    public GameObject prefabMap;
+    private GameObject mapInstance;
+    private bool prefabExists;
+
+    void Start()
+    {
+        prefabExists = false;
+        player.position = new Vector3(-400, -100, 0);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.position.x < -450 && player.position.x > -551)
+        Debug.Log(player.position.x);
+        if(prefabExists == false && player.position.x < -450 && player.position.x > -551)
         {
-            
+            mapInstance = Instantiate(prefabMap, mapGrid);
+            prefabExists = true;
+        }
+        else
+        {
+            if(prefabExists && (player.position.x > -450 || player.position.x < -551))
+            {
+                Destroy(mapInstance);
+                prefabExists = false;
+            }
         }
     }
 }
